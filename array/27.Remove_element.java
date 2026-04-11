e14b6b99dc14bca08aa662f9627246f7c4c8e744
Aditi <aditin7234@gmail.com>
2026-04-11 17:20:50 +0530

    Day 1: Remove element

---
+++ array/27.Remove_element.java
@@ -0,0 +1,16 @@
+//Problem: Remove element
+//Approach: Two pointer
+
+
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int k=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=val){
+                nums[k++]=nums[i];
+            }       
+        }
+        return k;
+        
+    }
+}
