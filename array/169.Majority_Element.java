699fb0b034181f47350524b302129ae87c63bbda
Aditi <aditin7234@gmail.com>
2026-04-18 11:50:23 +0530

    Day 4:Majority Element

---
+++ array/169.Majority_Element.java
@@ -0,0 +1,23 @@
+//Day 4: 169.Majority_Element
+//Approach: Moore voting algorithm
+
+class solution{
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        int candidate = 0;
+        
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            
+            if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        
+        return candidate;
+    }
+}
