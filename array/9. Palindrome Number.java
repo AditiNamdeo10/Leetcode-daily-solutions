c628ec9d64df6704ee05b983225f90a405d176bd
Aditi <aditin7234@gmail.com>
2026-06-07 20:05:23 +0530

    Solved

---
+++ array/9. Palindrome Number.java
@@ -0,0 +1,14 @@
+//Approach : 9. Palindrome Number
+
+class Solution {
+    public boolean isPalindrome(int x) {
+       
+        int n=x;
+        int rev=0;
+        while(n>0){
+            rev=rev*10+n%10;
+            n=n/10;
+        }
+        return x==rev;
+    }
+}
