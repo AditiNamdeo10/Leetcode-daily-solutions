63e622c172b98ed92cb5663cc30cd1fe6b9f3470
Aditi <aditin7234@gmail.com>
2026-06-09 10:30:43 +0530

    Optimized

--- array/1470. Shuffle the Array.java
+++ array/1470. Shuffle the Array.java
@@ -3,10 +3,9 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
         int [] ans= new int[n*2];
-        int j=0;
         int k=0;
         for(int i=0;i<n;i++){
-            ans[k++]=nums[j++];
+            ans[k++]=nums[i];
             ans[k++]=nums[n+i];
         }
         nums=ans;
