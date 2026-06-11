abd2621369da26969e2089d80b3e79f1c028186d
Aditi <aditin7234@gmail.com>
2026-06-11 17:27:43 +0530

    Solved

---
+++ array/1732. Find the Highest Altitude.java
@@ -0,0 +1,18 @@
+//Problem : Find the Highest Altitude
+
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int ans[]=new int[gain.length+1];
+        ans[0]=0;
+        for(int i=1; i<=gain.length; i++){
+            ans[i]=ans[i-1]+gain[i-1];
+        }
+        int max=ans[0];
+        for(int i=0; i<=gain.length; i++){
+            if(ans[i]>max){
+                max=ans[i];
+            }
+        }
+        return  max;
+    }
+}
