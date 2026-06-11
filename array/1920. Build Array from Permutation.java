149ddcf76668641a0ef1a09a3ad85c35b5d36017
Aditi <aditin7234@gmail.com>
2026-06-11 17:26:04 +0530

    Solved

---
+++ array/1920. Build Array from Permutation.java
@@ -0,0 +1,12 @@
+//Problem : 1920. Build array from permutation
+
+class Solution {
+    public int[] buildArray(int[] nums) {
+        int n=nums.length;
+        int[] ans=new int[n];
+        for(int i=0; i<n ; i++){
+            ans[i]=nums[nums[i]];
+        }
+        return ans;
+    }
+}
