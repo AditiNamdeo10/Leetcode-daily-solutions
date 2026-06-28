b19e556bb4cb7fbda039290a3595a50e2cfbbb8f
Aditi <aditin7234@gmail.com>
2026-06-28 22:07:49 +0530

    Solved

---
+++ array/7. Reverse Integer.java
@@ -0,0 +1,18 @@
+//Problem : 7. Reverse Integer
+
+class Solution {
+    public int reverse(int x) {
+        double ans=0;
+
+        
+        while(x!=0){
+            
+            ans=ans*10+x%10;
+            if(ans<=Integer.MIN_VALUE || ans>=Integer.MAX_VALUE){
+               return 0;
+            }
+            x=x/10;
+        }
+        return (int)ans;
+    }
+}
