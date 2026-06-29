77ee149df219f66fd3393fb1d22c22c291d7332f
Aditi <aditin7234@gmail.com>
2026-06-29 15:31:39 +0530

    Solved

--- array/1967. Number of Strings That Appear as Substrings in Word.py
+++ array/1967. Number of Strings That Appear as Substrings in Word.py
@@ -3,6 +3,7 @@ class Solution:
     def numOfStrings(self, patterns: List[str], word: str) -> int:
         c=0
         for i in patterns:
+            
             if i in word:
                 c=c+1
 
---
+++ array/28. Find the Index of the First Occurrence in a String.py
@@ -0,0 +1,8 @@
+# Problem : 28. Find the Index of the First Occurrence in a String
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if needle not in haystack:
+            return -1
+        return haystack.index(needle)
+        
