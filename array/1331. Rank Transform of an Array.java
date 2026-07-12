a8d4aadae617e50f14adea5a03d561e8103eb32d
Aditi <aditin7234@gmail.com>
2026-07-12 15:32:59 +0530

    Solved:

---
+++ array/1331. Rank Transform of an Array.java
@@ -0,0 +1,23 @@
+//Problem: 1331. Rank Transform of an Array
+//Topic: HashMap
+
+class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+        int [] sorted= arr.clone();
+        Arrays.sort(sorted);
+
+        HashMap<Integer,Integer>rank=new HashMap<>();
+        int r=1;
+        for(int num: sorted){
+            if(!rank.containsKey(num)){
+                rank.put(num,r);
+                r++;
+            }
+        }
+        int[] ans=new int[arr.length];
+        for(int i=0;i<arr.length;i++){
+            ans[i]=rank.get(arr[i]);
+        }
+        return ans;
+    }
+}
