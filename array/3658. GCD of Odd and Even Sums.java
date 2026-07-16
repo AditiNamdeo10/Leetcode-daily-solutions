7775c0d8cf58770a24a69e584735ab0aba0e7662
Aditi <aditin7234@gmail.com>
2026-07-16 09:15:00 +0530

    Solved

---
+++ array/3658. GCD of Odd and Even Sums.java
@@ -0,0 +1,26 @@
+//Problem : 3658. GCD of Odd and Even Sums.java
+//Greatest common divisor
+
+class Solution {
+    public int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+
+    //     int sumOdd=0;
+    //     int sumEven=0;
+    //     for(int i=1; i<(n*2)+1; i++){
+    //         if(i%2==0){
+    //             sumEven+=i;
+    //         }else{
+    //             sumOdd+=i;
+    //         }
+    //     }
+    //     int minele=Math.min(sumEven,sumOdd);
+    //     for(int i=minele; i>0;i--){
+    //         if(sumEven%i==0 && sumOdd%i==0){
+    //             return i;
+    //         } 
+    //     }
+    //     return 1;
+    // }
+}
