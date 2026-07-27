2a30a96d304778a4235369afd1a0b98bc577d923
Aditi <aditin7234@gmail.com>
2026-07-27 12:53:05 +0530

    Solved

---
+++ array/1464. Maximum Product of Two Elements in an Array.java
@@ -0,0 +1,17 @@
+// Problem : 1464. Maximum Product of Two Elements in an Array
+
+class Solution {
+    public int maxProduct(int[] nums) {
+        int first=Integer.MIN_VALUE;
+        int second=Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>first){
+                second=first;
+                first=nums[i];
+            }else if(nums[i]>second){
+                second=nums[i];
+            }
+        }
+        return ((first-1)*(second-1));
+    }
+}
